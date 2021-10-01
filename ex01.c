b36a129e47b3020d8cf7656f7bd5deb268258a31
Ítalo <italogholanda@gmail.com>
2021-10-01 16:25:57 -0300

    multi_i, multi_r

--- ex01.c
+++ ex01.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 19:49:39 by igomes-h          #+#    #+#             */
-/*   Updated: 2021/10/01 15:52:44 by igomes-h         ###   ########.fr       */
+/*   Updated: 2021/10/01 16:25:13 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,40 @@ int sum_r(int a, int b)
 	return (sum_r(++a, --b));
 }
 
+// Iterative
+int multi_i(int a, int b)
+{
+	int tmp;
+
+	tmp = 0;
+	while (b > 0)
+	{
+		b--;
+		tmp = sum_i(tmp,a);
+	}
+	return (tmp);
+}
+
+// Recursive
+int multi_r_aux(int a, int b, int m)
+{
+	if (a == 0 || b == 0)
+		return (m);
+	return (multi_r_aux(a, --b, sum_r(m,a)));
+}
+
+int multi_r(int a, int b)
+{
+	if (b < 0)
+		return (0);
+	return (multi_r_aux(a, b, 0));
+}
+
 int main()
 {
+	// SumI and SumR Tests
+	printf("%s", "SumI and SumR");
+	printf("%c", '\n');
 	// Test 1
 	printf("%i", sum_i(5, 6));
 	printf("%c", '\n');
@@ -63,5 +95,34 @@ int main()
 	printf("%i", sum_i(0, -1));
 	printf("%c", '\n');
 	printf("%i", sum_r(0, -1));
+	printf("%s", "\n--\n");
+
+	// MultiI and MultiR Tests
+	printf("%s", "SumI and SumR");
+	printf("%c", '\n');
+	// Test 1
+	printf("%i", multi_i(5, 5));
+	printf("%c", '\n');
+	printf("%i", multi_r(5, 5));
+	printf("%s", "\n--\n");
+	// Test 2
+	printf("%i", multi_i(-5, -5));
+	printf("%c", '\n');
+	printf("%i", multi_r(-5, -5));
+	printf("%s", "\n--\n");
+	// Test 3
+	printf("%i", multi_i(0, -5));
+	printf("%c", '\n');
+	printf("%i", multi_r(0, -5));
+	printf("%s", "\n--\n");
+	// Test 4
+	printf("%i", multi_i(28, 45));
+	printf("%c", '\n');
+	printf("%i", multi_r(28, 45));
+	printf("%s", "\n--\n");
+	// Test 5
+	printf("%i", multi_i(28, 0));
+	printf("%c", '\n');
+	printf("%i", multi_r(28, 0));
 	printf("%s", "\n--\n");
 }
